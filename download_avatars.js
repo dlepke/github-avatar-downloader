d2dbe90b1cc82344cb2b6cbd2d3830afac4f3a34
Deanna Lepke <deannalepke@gmail.com>
2017-09-11 21:58:47 +0000

    added requestURL to function

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,15 @@
+var GITHUB_USER = "dlepke";
+var GITHUB_TOKEN = '9593d7d9f7323577aa47f73559cdb3580cf759b5';
+
+
+
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  //...
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
