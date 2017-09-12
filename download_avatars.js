f0026622d0cec5cf5924ba59a4571b04a2fd763c
Deanna Lepke <deannalepke@gmail.com>
2017-09-12 00:35:33 +0000

    add console.log(body) to function

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,5 @@
 var GITHUB_USER = "dlepke";
-var GITHUB_TOKEN = '9593d7d9f7323577aa47f73559cdb3580cf759b5';
+var GITHUB_TOKEN = 'bd7d12aeca4d275ca9537c311c7aa1a9c37155dc';
 
 
 
@@ -7,13 +7,32 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'LhL Project #1',
+    }
+  }
+
+
+  request(options, function(err, response, body) {
+    if (err) {
+      console.log("inner inner inner function found an error; propagating it outward");
+      cb(err);
+      return;
+    }
+    // console.log(response);
+    console.log(body);
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+
+
+getRepoContributors('jquery', 'jquery', function(err, result) {
   console.log("Errors: ", err);
   console.log("Result: ", result);
 });
-
